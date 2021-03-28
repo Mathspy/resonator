666a8705ab9f6f48c86f859ecd11a60a60c817d0
Mathspy <mathspy257@gmail.com>
2021-03-29 01:58:34 +0200

    Updated script to point towards pupilfirst

--- run.sh
+++ run.sh
@@ -4,21 +4,21 @@ mkdir -p work
 mkdir -p results
 cd work
 
-git clone --depth=1 git@github.com:reazen/relude.git 
-cp -r relude relude_melange
-mkdir -p ../results/relude
+git clone --depth=1 git@github.com:pupilfirst/pupilfirst.git
+cp -r pupilfirst pupilfirst_melange
+mkdir -p ../results/pupilfirst
 
-cd relude
+cd pupilfirst
 npm install
-npx bsb -make-world 2>> ../../results/relude/avant.stderr 1>> ../../results/relude/avant.stdout
-echo $? > ../../results/relude/avant
+npx bsb -make-world 2>> ../../results/pupilfirst/avant.stderr 1>> ../../results/pupilfirst/avant.stdout
+echo $? > ../../results/pupilfirst/avant
 
-cd ../relude_melange
+cd ../pupilfirst_melange
 cp ../../esy.json .
 npm install
 rm -rf node_modules/bs-platform
 esy
-esy bsb -make-world 2>> ../../results/relude/melange.stderr 1>> ../../results/relude/melange.stdout
-echo $? > ../../results/relude/melange
+esy bsb -make-world 2>> ../../results/pupilfirst/melange.stderr 1>> ../../results/pupilfirst/melange.stdout
+echo $? > ../../results/pupilfirst/melange
 
 # TODO: Run Reason v4 on all .re files and see if it trips. If it does might be helpful to run v3.6 to see if it also trips since if it does likely an actual bug in someone's code that we can ignore
