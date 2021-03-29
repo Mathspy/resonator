99dae76e049de237e6f39cea6572c48d313ebbf3
Mathspy <mathspy257@gmail.com>
2021-03-29 02:29:42 +0200

    Always run with -clean-world

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ mkdir -p ../results/pupilfirst
 
 cd pupilfirst
 npm install
-npx bsb -make-world 2>> ../../results/pupilfirst/avant.stderr 1>> ../../results/pupilfirst/avant.stdout
+npx bsb -make-world -clean-world 2>> ../../results/pupilfirst/avant.stderr 1>> ../../results/pupilfirst/avant.stdout
 echo $? > ../../results/pupilfirst/avant
 
 cd ../pupilfirst_melange
@@ -23,7 +23,7 @@ cp ../../esy.json .
 npm install
 rm -rf node_modules/bs-platform
 esy
-esy bsb -make-world 2>> ../../results/pupilfirst/melange.stderr 1>> ../../results/pupilfirst/melange.stdout
+esy bsb -make-world -clean-world 2>> ../../results/pupilfirst/melange.stderr 1>> ../../results/pupilfirst/melange.stdout
 echo $? > ../../results/pupilfirst/melange
 
 # TODO: Run Reason v4 on all .re files and see if it trips. If it does might be helpful to run v3.6 to see if it also trips since if it does likely an actual bug in someone's code that we can ignore
